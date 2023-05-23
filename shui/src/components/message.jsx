9f4717eff1cbc8935808c990743bced2849e6e02
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-05-23 14:13:18 +0200

    push

---
+++ shui/src/components/message.jsx
@@ -0,0 +1,14 @@
+
+
+function Message() {
+
+    return (
+        <aside data-id='msg'>
+            <p data-id='msgDateTime'>Datum och tid</p>
+            <p data-id='msgText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti vitae quasi porro, cumque repellendus amet reiciendis asperiores quidem, magni, dolore qui a itaque sit reprehenderit voluptatibus culpa eaque. Dolorem, quaerat!</p>
+            <p data-id='msgUser'>Användarnamn</p>
+        </aside>
+    )
+}
+
+export default Message
