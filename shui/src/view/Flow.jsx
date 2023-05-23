ca18203fed03e0e02d5154adbcc0c1c244139d24
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-05-23 14:30:02 +0200

    igen

--- shui/src/view/Flow.jsx
+++ shui/src/view/Flow.jsx
@@ -1,11 +1,15 @@
-
+import { useNavigate } from "react-router-dom"
 
 function Flow() {
+    const navigate = useNavigate()
 
+    function handleClick() {
+        navigate('/writemsg')
+    }
     return (
         <article data-id='flow'>
             <h1>Här ska meddelanden komma</h1>
-            <button data-id='flowBtn'>skriv</button>
+            <button data-id='flowBtn' onClick={ handleClick }>skriv</button>
         </article>
     )
 }
