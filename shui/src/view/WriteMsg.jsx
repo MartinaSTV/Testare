8b2c716aaea1f66b60c6e24c1406606ba40198e3
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-05-23 14:29:44 +0200

    nav

--- shui/src/view/WriteMsg.jsx
+++ shui/src/view/WriteMsg.jsx
@@ -1,13 +1,27 @@
-
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 function WriteMsg() {
+    const navigate = useNavigate()
+    const [msg, setMsg] = useState()
+    const [user, setUser] = useState()
+    let timeDate;
+    let wholeMsg = {
+        user: user,
+        msg: msg,
+        timeDate: timeDate
+    }
+
+    function handleClick() {
+        
+        navigate('/', {state: {wholeMsg: wholeMsg}})
+    }
 
     return (
         <article data-id='writeMsg'>
-            <h1>meddelande</h1>
-            <input type="text" data-id='writeMsgText'/>
-            <input type="text" data-id='writeMsgUser'/>
-            <button data-id='writeMsgPost'>Publicera</button>
+            <input onChange={ (e) => setMsg(e.target.value) } type="text" data-id='writeMsgText'/>
+            <input onChange={ (e) => setUser(e.target.value) } type="text" data-id='writeMsgUser'/>
+            <button data-id='writeMsgPost' onClick={ handleClick }>Publicera</button>
         </article>
     )
 }
