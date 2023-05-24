75828d516f10e1512a11799e8c55f1725c9924db
MartinaSTV <foldermate@hotmail.com>
2023-05-24 09:56:12 +0200

    cimmit

--- shui/src/view/Flow.jsx
+++ shui/src/view/Flow.jsx
@@ -1,13 +1,26 @@
 import { useNavigate } from "react-router-dom"
 import './flow.scss'
 import ikon from '../assets/Subtractikon.svg'
+import { useState } from "react"
 
 function Flow() {
+
+    const [allMessages, setAllMessages] = useState()
+    console.log(allMsgs)
     const navigate = useNavigate()
 
     function handleClick() {
         navigate('/writemsg')
     }
+
+    useEffect(() => {
+        function getFromLocal() {
+            const localMessages = JSON.parse(localStorage.getItem('allMessages'))
+            setAllMessages(localMessages)
+        }
+        getFromLocal();
+    }, [])
+
     return (
         <article data-id='flow' className="flow">
             <section className="flow__rectangle"><img src={ikon} alt="ikon" /></section>
