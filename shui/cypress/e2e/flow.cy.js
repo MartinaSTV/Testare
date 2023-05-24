2c8d01998590a3a36758e3694da79da36d943ad9
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-05-24 09:38:53 +0200

    test

--- shui/cypress/e2e/flow.cy.js
+++ shui/cypress/e2e/flow.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    // Denna funktion körs varje gång innan varje test
+    cy.visit('http://localhost:5173/');
+  });
+  it('should show Stefans message first', () => {
+    cy.wait(1000)
+    cy.get("data-id='msgUser'").first().should('have.text', 'stefan')
+    cy.get("data-id='msgText'").first().should('have.text', 'hejsan')
   })
 })
